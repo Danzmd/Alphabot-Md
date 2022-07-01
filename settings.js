58bef87815b489c6de12979cb7cd7f620c27da39
Danzmd <107999522+Danzmd@users.noreply.github.com>
2022-07-01 15:01:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,9 +30,9 @@ global.ownernomerr = "+6289523573658" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","918156874290"] //ganti agar fitur owner bisa di gunakan
+global.owner = ["6289523573658","6289523573658","6289523573658"] //ganti agar fitur owner bisa di gunakan
 global.packname = '© Danzbot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh Danz' //sticker wm ganti nama kalian
+global.author = 'Di Buat Oleh DanzBot' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
